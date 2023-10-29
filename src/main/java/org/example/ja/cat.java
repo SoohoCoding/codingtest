746da5c9012b4620fa1dbf535a1047347190eb38
soohocoding <ckdgns1447@gmail.com>
2023-10-29 14:19:09 +0900

    test

---
+++ src/main/java/org/example/ja/cat.java
@@ -0,0 +1,11 @@
+package org.example.Ja;
+
+public class cat {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
