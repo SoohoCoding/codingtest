c62bf8d7cb3c619cd05a947f7a6699b4a060d82d
lch94 <ckdgns1447@gmail.com>
2024-03-05 12:30:15 +0900

    BaekJoon-입출력과 사칙연산

---
+++ src/main/java/org/example/ja/BaekJoon_10869.java
@@ -0,0 +1,17 @@
+package org.example.ja;
+
+public class BaekJoon_10869 {
+
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 3;
+
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
---
+++ src/main/java/org/example/ja/BaekJoon_10926.java
@@ -0,0 +1,16 @@
+package org.example.ja;
+
+import java.util.Scanner;
+
+public class BaekJoon_10926 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        System.out.println(a+"??!");
+
+    }
+
+}
